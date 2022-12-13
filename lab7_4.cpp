879e38809ba873869092ac2eaf1eac7200830bea
pubespubest <pubespubest@gmail.com>
2022-12-13 15:24:45 +0700

    lab7_4 complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -8,8 +8,16 @@ int main(){
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
-
+	int i=0;
+	double target_gpa;
     //Write your code here.
-		
+	cout<<"Please input GPA: ";
+	cin>>target_gpa;
+	while(i<10){
+		if(target_gpa<=gpa[i])
+			cout<<id[i]<<" "<<name[i]<<" "<<gpa[i]<<endl;
+		i++;
+	}
+
 	return 0;
 }
