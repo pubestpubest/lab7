027c3c239e683d3621a9c93557af0841f7b43854
pubespubest <pubespubest@gmail.com>
2022-12-13 15:10:19 +0700

    lab7_2 complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -5,10 +5,24 @@ using namespace std;
 int main(){
 	
 	int i = 0, P[5000], k, result;
+	cout<<"Please input k: ";
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cin>>k;
+	if (k>0){
+		i=0;
+		result=0;
+		while (i<5000){
+			if(P[i]%k==0)
+				result-=P[i];
+			else
+				result+=P[i];
+			i++;
+		}
+		cout<<"Result = "<<result;
+	}else
+		cout<<"Invalid input!!!";
 	return 0;
 }
