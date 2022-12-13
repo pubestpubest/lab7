f5b32b47c4a1f47dbd79251a482684ac05843ab0
pubespubest <pubespubest@gmail.com>
2022-12-13 15:18:06 +0700

    lab7_3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,23 @@
 
 using namespace std;
 
+int adiff(int a,int b){
+  int c;
+  c=(a%360)-(b%360);
+  if(c<0)
+    c=-c;
+  if (c>=180 or c<=-180)
+    c=360-c;
+  return c;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
+  cout << adiff(60,244)<<endl;
 }
